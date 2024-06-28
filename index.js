c08d6029faef409d21a78de2790ff3e0d519d70b
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-06-28 17:22:36 +0600

    mongoose, connect mongoDB

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+const uri = "mongodb://localhost:27017";
 
 
+mongoose.connect(uri,
+    ).then(()=>{
+        console.log('MongoDB Connected')})
+    .catch(()=>{
+    console.log('error')})
+
+const app = express();
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the blog-a-tet!');
+})
+
+app.post('/auth/login', (req, res) => {
+    console.log(req.body);
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: req.body.fullName,
+    }, 'secret123');
+
+    res.json ({
+       success: true,
+    })
+})
+
+app.listen(4444, (err) =>{
+ if (err) console.log(err);
+console.log('Server OK. Listening on port 4444')
+});
