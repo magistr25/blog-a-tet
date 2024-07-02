0964982afccc7a33ce8fd55bc6e45b391cb2c7d7
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-07-03 00:48:13 +0600

    configureStore

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
+import {Provider} from 'react-redux'
 import { BrowserRouter} from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
-
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -15,7 +16,9 @@ root.render(
     <CssBaseline />
     <ThemeProvider theme={theme}>
         <BrowserRouter>
-            <App />
+            <Provider store={store}>
+                <App />
+            </Provider>
         </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
