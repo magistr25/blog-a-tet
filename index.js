6667e89d01a6b4fbe9e20b0f305e1f98d460060c
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-07-02 15:48:04 +0600

    uploads, handleValidationErrors

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import checkAuth from './utils/checkAuth.js';
 
 import * as UserController from './controllers/UserController.js'
 import * as PostController from './controllers/PostController.js'
+import multer from "multer";
+import handleValidationErrors from "./utils/handleValidationErrors.js";
 
 // const uri = "mongodb+srv://magistr25:123@cluster0.ns1uise.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const uri = "mongodb://localhost:27017/test"
@@ -15,23 +17,47 @@ mongoose.connect(uri,
     console.log('error')})
 
 const app = express();
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ storage });
+
 app.use(express.json());
+app.use('/uploads', express.static('uploads') )
 
 // app.get('/', (req, res) => {
 //     res.send('Welcome to the blog-a-tet!');
 // })
 
-app.post('/auth/login', loginValidation, UserController.login);
-
-app.post('/auth/register/', registerValidation, UserController.register);
-
+app.post('/auth/login', loginValidation, handleValidationErrors,  UserController.login);
+app.post('/auth/register/', loginValidation, handleValidationErrors, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe)
+
+app.post('/upload', checkAuth, upload.single('image'),
+    (req, res) => {
+        res.json({
+            url: `./uploads/${req.file.originalname}`,
+        })
+    })
 
 app.get('/posts', PostController.getAll)
 app.get('/posts/:id', PostController.getOne)
 app.delete('/posts/:id', checkAuth, PostController.remove)
-app.patch('/posts/:id', checkAuth, PostController.update)
+app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update)
 app.post('/posts', checkAuth, postCreateValidation, PostController.create)
+
+app.use((req, res, next) => {
+    res.setHeader("Content-Security-Policy", "default-src 'self'; img-src 'self' data: http://localhost:4444; script-src 'self'; style-src 'self'");
+    next();
+});
 
 app.listen(4444, (err) =>{
  if (err) console.log(err);
